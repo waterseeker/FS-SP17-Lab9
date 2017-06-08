1796ea787e002900901c77618e4ab034a44bad7c
Wayne Burris <waterseeker@hotmail.com>
2017-06-08 12:14:47 -0500

    basics working

---
+++ client/scripts.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+    getData();
+    
+    $('button').prop('disabled', true); //disables the chirp button on page load
+    $("#inputChirp").keypress(function () { //when user presses a key inside the chirp field
+        $('button').prop('disabled', false); // enable the button
+    });
+    //on  button click, empty the input field and disable the button
+    $("#btn").click(function () { //when the button is clicked
+        postData(); //call the postData function
+        $('input[type="text"]').val(''); //clear the input field
+        $('button').prop('disabled', true); //disable the chirp button
+    })
+
+    function postData() {
+        var newChirp = { //creates a newChirp object with the fields...
+            message: $('#inputChirp').val(), //message, that gets the content of the chirp field
+            user: "userName", //userName that gets a default right now
+            timestamp: new Date().toISOString() //and a timestamp for when it was creaeted.
+        }
+
+        // console.log('hello');
+
+        $.ajax({
+            method: "POST", 
+            url: 'http://localhost:3000/api/chirps',
+            contentType: 'application/json',
+            data: JSON.stringify(newChirp)
+        })
+            .then(function (success) {
+                console.log("APPENDING");
+                $('<div class="chirp"></div>').text(newChirp.message).appendTo(
+                    $("#posts")
+
+                ), function (err) {
+                    console.log(err);
+                };
+            })
+            .fail(function (xhr, status, error) {
+                console.log(error);
+            });
+    }
+
+
+
+    function getData() {
+        console.log("Front end is GETTING data from the server");
+        $.ajax({
+            method: 'GET',
+            url: 'http://localhost:3000/api/chirps',
+            contentType: 'application/json'
+        }).then(function (success) {
+            // console.log(success); test to see the new chirps object
+            $('#posts').empty();
+            for (i = 0; i < success.length; i++) {
+                $('<div class="chirp"></div>').text(success[i].message + "   -" + success[i].user + "-").appendTo($('#posts'));
+            }
+        })
+    }
+
+
+
+});
--- server/index.js
+++ server/index.js
@@ -1,38 +1,133 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-console.log("test");
-
+var path = require('path');
+//creates event listener basically for server
 var server = http.createServer(function (req, res) {
-    var q = url.parse(req.url, true); //uses the parse method on the url passed as the req.
-    var filename = "." + q.pathname; //turns the pathname in the url to a ".pathname"
-    fs.readFile(filename, function (err, data) {
-        if (err) { //if the requested file doesn't exist
-            res.writeHead(404, { 'Content-Type': 'text/html' }); //if the file isn't found, throw a 404
-            return res.end("404 Not Found");//send 404 and exit the function
-        } else if (filename === '/') { //if the request ends with '/'
-            res.writeHead(200, { 'Content-Type': 'text/html' }); //return ok code (200), set content
-            //type to html
-            res.write(../../client / index.html); //write from the index.html file in the client dir
-            return res.end();//send the file and exit the function
-        } else if (filename === '/api/chirps') { //if the requested filename is '/api/chirps'
-            if (req.method === 'GET') { //check to see if the request method is a GET request
-                fs.readFile('/server/data.json', 'utf8', function (err, data) { //read the data.json in the server folder
-                    if (err) {
-                        return console.log(err);
-                    }
-                    console.log(data);
-                });
-                res.writeHead(200, { 'Content-Type': 'JSON' });//set response code to 200 and content type to JSON
-                return res.end()//respond with the JSON data from the file and exit the function
-            } else if (req.method === 'POST') { //if the request is a POST
-                fs.readFile('/server/data.json', 'utf8', function (err, data) {
-                    if (err) {
-                        return console.log(err);
-                    }
-                    console.log(data);
-                });
+    //breaks the URL up into pieces so I can use them
+    var parsedURL = url.parse(req.url, true);
+    // console.log(parsedURL);
+    //if the URL path is a slash
+    console.log(parsedURL.pathname);
+    if (parsedURL.pathname == "/") {
+        //sets the var to the path name that leads to index.html
+        var htmlPath = path.join(__dirname, "..", "client", "index.html");
+        //if the filepath is the var
+        fs.readFile(htmlPath, function (err, data) {
+            if (err) {
+                console.log("Couldn't load the index.html page");
+            } else {
+                //write to the head 200 (ok) and return this content type
+                res.writeHead(200, { "Content-Type": "text/html" });
+                //write to the document
+                res.write(data);
+                //end
+                res.end();
             }
         });
+    }
+    else if (parsedURL.pathname == "/scripts.js") {
+        var jsPath = path.join(__dirname, "..", "client", "scripts.js");
+        fs.readFile(jsPath, function (err, data) {
+            if (err) {
+                console.log("Couldn't load the scripts.js file.");
+            } else {
+                res.writeHead(200, {
+                    "Content-Type": "application/javascript"
+                });
+                res.write(data);
+                res.end();
+            }
+        })
+    }
+    else if (parsedURL.pathname == "/styles.css") {
+        var cssPath = path.join(__dirname, "..", "client", "styles.css");
+        fs.readFile(cssPath, function (err, data) {
+            if (err) {
+                console.log("Couldn't load the CSS file.");
+            } else {
+                res.writeHead(200, {
+                    "Content-Type": "text/css"
+                });
+                res.write(data);
+                res.end();
+            }
+        })
+    }
+    else if (parsedURL.pathname == "/api/chirps") {
+        var filePath = path.join(__dirname, "..", "server", "data.json");
+        if (req.method === "GET") {
+            fs.readFile(filePath, function (err, data) {
+                if (err) {
+                    console.log("Couldn't load the data.json file");
+                } else {
+                    res.writeHead(200, {
+                        "Content-Type": "application/json"
+                    });
+                    res.write(data);
+                    res.end();
+                }
+            })
+        }
+    }
+    else if (parsedURL.pathname == "/resources/chirper-background.jpg") {
+        console.log("Yep ye");
+        var imgPath = path.join(__dirname, "..", "client", "resources", "chirper-background.jpg");
+        fs.readFile(imgPath, function (err, data) {
+            if (err) {
+                console.log("Couldn't load the background image file.");
+            } else {
+                res.writeHead(200, {
+                    "Content-Type": "image/jpeg"
+                });
+                res.write(data);
+                res.end();
+            }
+        })
+    }
+    else if (req.method === "POST") {
+        console.log('making a post request');
+        //set empty value for the new chirp
+        var newChirp = '';
+        //when you get the new data from the post, and convert to JS object
+        req.on('data', function (json) {
+            newChirp = JSON.parse(json);
+        });
+        //when you end this, 
+        req.on('end', function () {
+            console.log('POSTing to the front end');
+            fs.readFile(filePath, function (err, data) {
+                console.log(filePath);
+                if (err) {
+                    console.log("POST to front end failed");
+                } else {
+                    // console.log(data); test to make sure it's grabbing the chirps
+                    //read the file, get the entire array and push the new chirp to it
+                    var jsonData = JSON.parse(data.toString('utf-8').trim());
+                    //pushes new chirp to the array
+                    jsonData.push(newChirp);
+                    //make array JSON again
+                    var finalChirp = JSON.stringify(jsonData);
+                    //then write the new stuff to the file
+                    fs.writeFile(filePath, finalChirp, 'utf-8', function (err) {
+                        var code = 201,
+                            header = {
+                                'Content-Type': 'application/json'
+                            };
+                        res.writeHead(code, header);
+                        res.end(finalChirp);
+                    });
+                }
+            });
+        })
+    }
+    else {
+    console.log("Nope, couldn't find: " + parsedURL.pathname)
+    res.writeHead(404, {
+        "Content-Type": "text/plain"
+    });
+    res.write("Not Found");
+    res.end();
+}
 });
 server.listen(3000);
